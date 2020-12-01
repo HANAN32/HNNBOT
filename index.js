f5b014780af51201e15b6ad17f4783035eeafc76
HANAN32 <75285742+HANAN32@users.noreply.github.com>
2020-12-02 05:35:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const  BotName  =  'HNNBOT':
+const  instagram  =  'https://t.me/jangkrjk' ;:
+const  telegram  =  'https://t.me/jangkrjk' ;
+const kapanbotaktif = '06.00-19.00'; 
+const youtube = 'https://youtu.be/-lSjxAtMxms';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
